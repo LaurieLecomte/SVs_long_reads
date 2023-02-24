cd944d4c019d73e9153f8d5580390cb490988fd5
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2023-02-24 14:55:17 -0500

    removed unecessary sorting step

--- 01_scripts/01.1_sniffles_call.sh
+++ 01_scripts/01.1_sniffles_call.sh
@@ -24,10 +24,10 @@ then
 fi
 
 # 1. Call SVs in whole genome
-sniffles --input $BAM_DIR/"$SAMPLE".bam --vcf $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE"_all_contigs.vcf.gz --snf $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE".snf --threads $CPU --reference $GENOME --sample-id $SAMPLE --output-rnames --combine-consensus --allow-overwrite
+#sniffles --input $BAM_DIR/"$SAMPLE".bam --vcf $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE"_all_contigs.vcf.gz --snf $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE".snf --threads $CPU --reference $GENOME --sample-id $SAMPLE --output-rnames --combine-consensus --allow-overwrite
 
 # 2. Sort, remove SVs where END is < than POS (usually happens if a SV is at POS 1 on an uplaced contig) and remove unplaced contigs
-bcftools sort $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE"_all_contigs.vcf.gz | bcftools filter -e "POS > INFO/END" | bcftools view -R $CHR_BED > $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE".vcf
+bcftools view -R $CHR_BED $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE"_all_contigs.vcf.gz | bcftools filter -e "POS > INFO/END" > $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE".vcf
 
-# 3. Filter for PASS and PRECISE calls and remove BNDs and INVDUPs
-bcftools filter -i 'FILTER="PASS" & PRECISE=1 & SVTYPE!="BND" & SVTYPE!="INVDUP"' $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE".vcf > $CALLS_DIR/sniffles/"$SAMPLE"_PASS_PRECISE.vcf
+# 3. Filter for PASS and PRECISE calls and remove BNDs and INVDUPs and SVs supported by less than 2 reads
+bcftools filter -i 'FILTER="PASS" & PRECISE=1 & SVTYPE!="BND" & SVTYPE!="INVDUP" & SUPPORT > 1' $CALLS_DIR/sniffles/$SAMPLE/"$SAMPLE".vcf > $CALLS_DIR/sniffles/"$SAMPLE"_PASS_PRECISE.vcf

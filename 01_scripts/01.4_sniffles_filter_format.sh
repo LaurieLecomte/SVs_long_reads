fdfb98d209298da6686fed3434b3851d1690e55c
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2023-02-27 18:39:34 -0500

    added step for renaming samples prior to across callers merge

--- 01_scripts/01.4_sniffles_filter_format.sh
+++ 01_scripts/01.4_sniffles_filter_format.sh
@@ -19,10 +19,22 @@ FILT_DIR="07_filtered"
 CPU=1
 
 
-# 1. Re-filter for PASS, PRECISE, number of supporting reads > 1 and wanted SVs types, then simplify VCF fields and sort
-bcftools filter -i 'FILTER="PASS" & PRECISE=1 & SVTYPE!="BND" & SVTYPE!="INVDUP"' $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.vcf | bcftools annotate -x ^INFO/SVTYPE,INFO/SVLEN,INFO/END,INFO/OLDTYPE | bcftools sort > $FILT_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2.vcf
+# 1. Prepare file with new read names
+## Extract old names
+bcftools query -l $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.vcf > $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.old_names
 
+## Extract new names
+sed -E 's/[0-9]+\_([A-Za-z0-9]+)/\1/' $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.old_names > $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.new_names
+
+## Paste old and new names in a single file to supply to bcftools reheader
+paste -d "\t" $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.old_names $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.new_names > $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.old_new_names
+
+# 2. Re-filter for PASS, number of supporting reads > 1 and wanted SVs types, then simplify VCF fields and sort
+bcftools filter -i 'FILTER="PASS" & SVTYPE!="BND"' $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.vcf | bcftools annotate -x ^INFO/SVTYPE,INFO/SVLEN,INFO/END,INFO/OLDTYPE,INFO/IRIS_REFINED | bcftools reheader -s $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.old_new_names | bcftools sort > $FILT_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2.vcf
 
 # Clean up 
 rm $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined_dupToIns.vcf
 rm $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined_noGenotypes.vcf
+rm $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.old_names
+rm $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.new_names
+rm $MERGED_DIR/sniffles/sniffles_PASS_PRECISE_RSUPP2_refined.old_new_names
